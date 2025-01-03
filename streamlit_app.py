3ee81e0f76d983df7a1593275646c100dbd6f871
NetraHugar <netra.hugar@in.ibm.com>
2025-01-03 16:11:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,11 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+cnx=st.connection("snowflake")
+session=cnx.session()
+my_dataframe=session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 
-ingredients_list = st.multiselect (
+ingredients_list=st.multiselect (
     'Choose upto 5 ingredients:'
     , my_dataframe
 )
@@ -29,9 +29,9 @@ if ingredients_list:
       ingredients_string += fruit_chosen + ' '
       st.subheader(fruit_chosen + 'Nutrition Information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
-      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+      sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+my_insert_stmt=""" insert into smoothies.public.orders(ingredients, name_on_order)
                  values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
 time_to_insert = st.button('Submit Order')
